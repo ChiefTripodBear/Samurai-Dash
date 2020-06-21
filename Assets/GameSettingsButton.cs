239e6e8abab8aca3dc749e6fc5b48f01ff552a1a
conholo <47753963+conholo@users.noreply.github.com>
2020-06-21 19:05:22 -0400

    Added sweet font
    Get over it

--- Assets/GameSettingsButton.cs
+++ Assets/GameSettingsButton.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class GameSettingsButton : MonoBehaviour
 {
     [SerializeField] private GameObject _pauseScreenButtonsHolder;
+    [SerializeField] private string _activeText = "Return";
+    [SerializeField] private string _inactiveText = "Settings";
+    [SerializeField] private TMP_Text _settingsButtonText;
     
     public static event Action<bool> OnTogglePause;
 
@@ -19,6 +23,8 @@ public class GameSettingsButton : MonoBehaviour
         _pauseScreenActive = !_pauseScreenActive;
 
         OnTogglePause?.Invoke(_pauseScreenActive);
+        
+        _settingsButtonText.SetText(_pauseScreenActive ? _activeText : _inactiveText);
 
         _pauseScreenButtonsHolder.SetActive(_pauseScreenActive);
     }
